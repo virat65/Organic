cf398a52c6e52a5e0d031bbbca40238945a0ec87
virat65 <virat1322420@gmail.com>
2025-08-17 20:23:31 +0530

    fileupload and DataBase placed on their places  in server.js

--- organic_backend/server.js
+++ organic_backend/server.js
@@ -6,12 +6,12 @@ import fileUpload from "express-fileupload";
 const app = express();
 dotenv.config();
 const port = process.env.PORT;
-app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 dataBase();
+app.use(express.json());
+app.use(fileUpload())
+// app.use(express.urlencoded({ extended: true }));
 
 app.use("/api",userRouter)
-app.use(fileUpload())
 app.listen(port, () => {
   console.log(`server is running on the ${port}`);
 });
