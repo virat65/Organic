0ccc892a59148dd7b957222d8f7d100135e485d1
virat65 <virat1322420@gmail.com>
2025-08-17 19:34:38 +0530

    usermodel upated

--- organic_backend/Model/userModel.js
+++ organic_backend/Model/userModel.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
-const userSchema =new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: { type: String, default: "" },
     password: { type: String, default: "" },
     email: { type: String, default: "" },
     phone: { type: Number, default: 0 },
     image: { type: String, default: "" },
-    token:{type:String,default:""},
+    token: { type: String, default: "" },
     logintime: { type: Number, default: 0 },
   },
   {
     timestamps: true,
   }
 );
-const userModel =new mongoose.model("/user", userSchema);
+const userModel = new mongoose.model("user", userSchema);
 export default userModel;
